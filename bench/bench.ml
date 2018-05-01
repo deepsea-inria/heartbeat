20983e2521a3d5033de180ab6de0966d05d65a34
Mike Rainey <bionic.oldsmobile@gmail.com>
2018-05-01 15:53:12 -0400

    temporarily limiting nb threads during build phase to prevent hanging machine

--- bench/bench.ml
+++ bench/bench.ml
@@ -73,7 +73,7 @@ let nothing () = ()
 (** Files and binaries *)
 
 let build path bs is_virtual =
-   system (sprintf "make -C %s -j %s" path (String.concat " " bs)) is_virtual
+   system (sprintf "make -C %s -j12 %s" path (String.concat " " bs)) is_virtual
 
 let file_results exp_name =
   Printf.sprintf "results_%s.txt" exp_name
